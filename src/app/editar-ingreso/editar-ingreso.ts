70dc13e37660eb48b61e620bd1a7e642b5aafcea
jhormanquina17-arch <jhormanquina17@gmail.com>
2026-08-05 15:43:39 -0500

    componentes de crear, listar,editar

---
+++ src/app/editar-ingreso/editar-ingreso.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-ingreso',
+  imports: [],
+  templateUrl: './editar-ingreso.html',
+  styleUrl: './editar-ingreso.css',
+})
+export class EditarIngreso {
+
+}
