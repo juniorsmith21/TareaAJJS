8a7510194ae2c8feb92d6839f366e718af639020
JDquintero1002 <jdquintero@unicauca.edu.co>
2026-08-06 16:55:09 -0500

    lo mio

--- src/app/usuarios/usuarios.ts
+++ src/app/usuarios/usuarios.ts
@@ -7,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrl: './usuarios.css',
 })
 export class Usuarios {
+  nuevoUsuario={
+  id_usuario:'',
+  contrasena:'',
+  tipo_usuario:'',
+  estado:'',
+  registrar:false,
+  editar:false,
+  eliminar:false,
+  id_funcionario:''
+}
+
+// arreglo del GET,  se guardaran las respuestas del GET
+
+funcionarios: any[] = [];
+
+//Hacer get de funcionarios
+obtenerFuncionarios(){
+  this.http.get<any[]>("https://TU_PROYECTO.supabase.co/rest/v1/funcionario?select=*", 
+    {
+      Headers:
+    }
+  )
+}
 
 }
+
