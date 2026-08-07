b4f4d6f5ddd7c44182d3d2e23676380a1804a325
jhormanquina17-arch <jhormanquina17@gmail.com>
2026-08-07 17:48:47 -0500

    ingreso completo

--- src/app/editar-ingreso/editar-ingreso.ts
+++ src/app/editar-ingreso/editar-ingreso.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-ingreso',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './editar-ingreso.html',
-  styleUrl: './editar-ingreso.css',
+  styleUrl: './editar-ingreso.css'
 })
 export class EditarIngreso {
+
+  ingresoEditar = {
+    id_ingreso: '',
+    fecha_ingreso: '',
+    fecha_salida: '',
+    estado: '',
+    id_usuario: ''
+  }
+
+  constructor(private http: HttpClient) {}
+
+  actualizarIngreso() {
+
+    this.http.patch(
+      "https://mcylhpmszpjvcohpzqgy.supabase.co/rest/v1/ingreso?id_ingreso=eq.1" + this.ingresoEditar.id_ingreso,
+      {
+        fecha_ingreso: this.ingresoEditar.fecha_ingreso,
+        fecha_salida: this.ingresoEditar.fecha_salida,
+        estado: this.ingresoEditar.estado,
+        id_usuario: this.ingresoEditar.id_usuario
+      },
+      {
+        headers: {
+          Authorization: "Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x",
+          apikey: "sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x",
+          "Content-Type": "application/json"
+        }
+      }
+    ).subscribe({
+      next: (respuesta) => {
+        alert("Ingreso actualizado"+ respuesta);
+      },
+      error: (error) => {
+        alert("Error al actualizar");
+        console.log(error);
+      }
+    });
+
+  }
 
 }
--- src/app/ingreso/ingreso.ts
+++ src/app/ingreso/ingreso.ts
@@ -1,11 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ingreso',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './ingreso.html',
-  styleUrl: './ingreso.css',
+  styleUrl: './ingreso.css'
 })
 export class Ingreso {
+
+  ingresos:any[]=[];
+
+  constructor(private http:HttpClient){}
+
+  ngOnInit(){
+    this.obtenerIngresos();
+  }
+
+  obtenerIngresos(){
+
+    this.http.get<any[]>(
+      "https://srrpeanqjqfxtnuwhjez.supabase.co/rest/v1/ingreso?select=*",
+      {
+        headers:{
+          Authorization:"Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x",
+          apikey:"sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x"
+        }
+      }
+    ).subscribe({
+      next:(respuesta)=>{
+        this.ingresos=respuesta;
+      }
+    });
+
+  }
 
 }
